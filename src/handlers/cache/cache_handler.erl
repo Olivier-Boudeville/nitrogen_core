b3d80bc9157ff7b159a133010a0a687089626d62
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-05-09 18:22:51 +0200

    Adding the support of multiple content roots.

--- src/handlers/cache/cache_handler.erl
+++ src/handlers/cache/cache_handler.erl
@@ -1,4 +1,3 @@
-% vim: sw=4 ts=4 et ft=erlang
 % Nitrogen Web Framework for Erlang
 % Copyright (c) 2008-2010 Rusty Klophaus
 % See MIT-LICENSE for licensing information.
@@ -6,70 +5,104 @@
 -module (cache_handler).
 -include("wf.hrl").
 -export ([
-    get_cached/3,
-    set_cached/3,
-    clear/1,
-    clear_all/0
+	get_cached/3,
+	set_cached/3,
+	clear/1,
+	clear_all/0
 ]).
 
--callback init(         Config :: term(),
-                        State :: term()) -> {ok, State :: term() }.
--callback finish(       Config :: term(), 
-                        State :: term()) -> {ok, State :: term() }.
--callback get_cached(   Key :: term(),
-                        Function :: fun(),
-                        TTL :: infinity | integer(),
-                        Config :: term(),
-                        State :: term()) -> {ok, Value :: any(), State :: term()}.
--callback set_cached(   Key :: term(),
-                        Value :: term(),
-                        TTL :: infinity | integer(),
-                        Config :: term(),
-                        State :: term()) -> {ok, State :: term()}.
--callback clear(        Key :: term(),
-                        Config :: term(),
-                        State :: term()) -> {ok, State :: term()}.
--callback clear_all(    Config :: term(),
-                        State :: term()) -> {ok, State :: term()}.
 
-% @doc get_cached(Key, Function, TTL, State) -> {ok, Value, NewState}
-% Return the cache value associated with Key. If it is not found,
-% then run the Function, store the resulting value in cache under
-% Key, and return the value.
--spec get_cached(Key :: any(),
-                 Function :: function(),
-                 TTL :: integer() | undefined | infinity) -> {ok, term()}.
-get_cached(Key, Function, TTL) ->  
-    ContextedFun = fun() ->
-        %% capture existing caching state before evaluating
-        Caching = wf_context:caching(),
-    
-        %% let us know we're caching
-        wf_context:caching(true),
+-type cache_state() :: term().
 
-        %% Process the caching function
-        Result = Function(),
 
-        %% Restore the caching status to its previous state (and if the state is unchanged, save some cycles by skipping it
-        ?WF_IF(not(Caching), wf_context:caching(Caching)),
+-callback init( Config :: term(), CacheState :: cache_state() ) ->
+	{ok, CacheState :: cache_state() }.
 
-        %% Return the result
-        Result
-    end,
-    {ok, _Value} = wf_handler:call(cache_handler, get_cached, [Key, ContextedFun, TTL]).
+-callback finish( Config :: term(), CacheState :: cache_state() ) ->
+	{ok, CacheState :: cache_state() }.
 
-% @doc set_cached(Key, Value, TTL, State) -> {ok, NewState}
+-callback get_cached( Key :: term(), Function :: fun(),
+					  TTL :: infinity | integer(), Config :: term(),
+					  CacheState :: cache_state() ) ->
+	{ok, Value :: any(), CacheState :: cache_state()}.
+
+-callback set_cached( Key :: term(), Value :: term(),
+					  TTL :: infinity | integer(), Config :: term(),
+					  CacheState :: cache_state()) ->
+	{ok, CacheState :: cache_state()}.
+
+
+-callback clear( Key :: term(), Config :: term(),
+				 CacheState :: cache_state()) ->
+	{ok, CacheState :: cache_state()}.
+
+-callback clear_all( Config :: term(), CacheState :: cache_state()) ->
+	{ok, CacheState :: cache_state()}.
+
+
+-spec init( Config :: term(), CacheState :: cache_state() ) ->
+	{ok, CacheState :: cache_state() }.
+init( Config, InitialCacheState ) ->
+
+	trace_utils:debug_fmt( "Initialising cache handler from configuration ~p "
+		"and initial cache state ~p.", [ Config, InitialCacheState ] ),
+
+	{ok, InitialCacheState }.
+
+
+
+% Returns the cache value associated with Key. If it is not found, then run the
+% Function, store the resulting value in cache under Key, and return the value.
+%
+-spec get_cached( Key :: any(), Function :: function(),
+				  TTL :: integer() | undefined | infinity) -> {ok, term()}.
+get_cached(Key, Function, TTL) ->
+
+	ContextedFun = fun() ->
+
+		%% capture existing caching state before evaluating
+		Caching = wf_context:caching(),
+
+		%% let us know we're caching
+		wf_context:caching(true),
+
+		%% Process the caching function
+		Result = Function(),
+
+		%% Restore the caching status to its previous state (and if the state is unchanged, save some cycles by skipping it
+		?WF_IF(Caching, wf_context:caching(Caching)),
+
+		%% Return the result
+		Result
+
+	end,
+
+	P = {ok, Value} = wf_handler:call( cache_handler, get_cached,
+										[Key, ContextedFun, TTL]),
+
+	trace_utils:debug_fmt( "[~w] Getting cached value for key '~p': '~p'.",
+						   [ self(), Key, Value ] ),
+
+	P.
+
+
 % Set the cached value directly without checking if it currently exists. If a
 % value already exists for that key, it is replaced.
 set_cached(Key, Value, TTL ) ->
-    ok = wf_handler:call(cache_handler, set_cached, [Key, Value, TTL]).
+
+	trace_utils:debug_fmt( "Setting cached value for key '~p' to ~p",
+						   [ Key,Value ] ),
+
+	ok = wf_handler:call(cache_handler, set_cached, [Key, Value, TTL]).
+
 
 % @doc Remove a value from cache.
 -spec clear(Key :: any()) -> ok.
-clear(Key) ->	
-    ok = wf_handler:call(cache_handler, clear, [Key]).
+clear(Key) ->
+	ok = wf_handler:call(cache_handler, clear, [Key]).
+
 
 % @doc Clear all values from cache.
 -spec clear_all() -> ok.
-clear_all() -> 
-    ok = wf_handler:call(cache_handler, clear_all).
+clear_all() ->
+	ok = wf_handler:call(cache_handler, clear_all).
--- src/handlers/cache/default_cache_handler.erl
+++ src/handlers/cache/default_cache_handler.erl
@@ -1,4 +1,3 @@
-% vim: sw=4 ts=4 et ft=erlang
 % Nitrogen Web Framework for Erlang
 % Copyright (c) 2015-2020 Jesse Gumm
 % See MIT-LICENSE for licensing information.
@@ -7,71 +6,97 @@
 %
 
 -module (default_cache_handler).
+
 -behaviour (cache_handler).
+
 -include("wf.hrl").
+
+
+% Corresponding to the base content root of the Nitrogen-based website
+% corresponding to this cache instance:
+%
+-type cache_state() :: file_utils:bin_directory_path().
+
+
 -export ([
-    init/2, 
-    finish/2,
-    get_cached/5,
-    set_cached/5,
-    clear/3, 
-    clear_all/2
+	init/2,
+	finish/2,
+	get_cached/5,
+	set_cached/5,
+	clear/3,
+	clear_all/2
 ]).
 
-init(Config, State) ->
-    CacheName = cache_name(Config),
-    try maybe_add_cache(CacheName)
-    catch error:undef ->
-        wf:error("nitro_cache not found in the system. You should add it as a dependency to your rebar.config like this:~n    ~p~n", [{nitro_cache, {git, "git://github.com/nitrogen/nitro_cache", {branch, master}}}]),
-        throw({nitro_cache_not_found, "See message above"})
-    end,
-    {ok, State}.
+
+init( Config, InitialCacheState ) ->
+
+	CacheName = cache_name( Config ),
+
+	trace_utils:debug_fmt( "Initialising default cache handler from "
+		"configuration '~p' and initial cache state ~p (cache name is '~ts').",
+		[ Config, InitialCacheState, CacheName ] ),
+
+	try
+
+		maybe_add_cache(CacheName)
+
+	catch error:undef ->
+
+		wf:error("nitro_cache not found in the system. You should add it as a dependency to your rebar.config like this:~n    ~p~n", [{nitro_cache, {git, "git://github.com/nitrogen/nitro_cache", {branch, master}}}]),
+
+		throw({nitro_cache_not_found, "See message above"})
+
+	end,
+
+	{ok, InitialCacheState}.
+
+
 
 maybe_add_cache(CacheName) ->
-    case application:get_env(nitro_cache, initialized_nitrogen_caches) of
-        undefined ->
-            add_cache([], CacheName);
-        {ok, Caches} ->
-            case lists:member(CacheName, Caches) of
-                true -> ok;
-                false -> add_cache(Caches, CacheName)
-            end
-    end.
+	case application:get_env(nitro_cache, initialized_nitrogen_caches) of
+		undefined ->
+			add_cache([], CacheName);
+		{ok, Caches} ->
+			case lists:member(CacheName, Caches) of
+				true -> ok;
+				false -> add_cache(Caches, CacheName)
+			end
+	end.
 
 add_cache(Caches, CacheName) ->
-    try nitro_cache:init(CacheName)
-    catch error:badarg ->
-        case nitro_cache:cache_exists(CacheName) of
-            true -> ok;
-            false -> throw({cannot_init_cache, CacheName})
-        end
-    end,
-    application:set_env(nitro_cache, initialized_nitrogen_caches, [CacheName | Caches]).
+	try nitro_cache:init(CacheName)
+	catch error:badarg ->
+		case nitro_cache:cache_exists(CacheName) of
+			true -> ok;
+			false -> throw({cannot_init_cache, CacheName})
+		end
+	end,
+	application:set_env(nitro_cache, initialized_nitrogen_caches, [CacheName | Caches]).
 
-finish(_Config, State) -> 
-    {ok, State}.
+finish(_Config, State) ->
+	{ok, State}.
 
 -spec get_cached(term(), fun(), integer() | infinity, proplist(), any()) -> {ok, term(), any()}.
 get_cached(Key, Function, TTL, Config, State)
-        when is_function(Function, 0) -> 
-    CacheName = cache_name(Config),
-    Return = nitro_cache:get(CacheName, TTL, Key, Function),
-    {ok, Return, State}.
+		when is_function(Function, 0) ->
+	CacheName = cache_name(Config),
+	Return = nitro_cache:get(CacheName, TTL, Key, Function),
+	{ok, Return, State}.
 
 set_cached(Key, Value, TTL, Config, State) ->
-    CacheName = cache_name(Config),
-    nitro_cache:set(CacheName, TTL, Key, Value),
-    {ok, State}.
+	CacheName = cache_name(Config),
+	nitro_cache:set(CacheName, TTL, Key, Value),
+	{ok, State}.
 
-clear(Key, Config, State) -> 
-    nitro_cache:flush(cache_name(Config), Key),
-    {ok, State}.
+clear(Key, Config, State) ->
+	nitro_cache:flush(cache_name(Config), Key),
+	{ok, State}.
 
-clear_all(Config, State) -> 
-    nitro_cache:flush(cache_name(Config)),
-    {ok, State}.
+clear_all(Config, State) ->
+	nitro_cache:flush(cache_name(Config)),
+	{ok, State}.
 
 cache_name(undefined) ->
-    nitrogen;
+	nitrogen;
 cache_name(Config) ->
-    proplists:get_value(cache_name, Config, nitrogen).
+	proplists:get_value(cache_name, Config, nitrogen).
